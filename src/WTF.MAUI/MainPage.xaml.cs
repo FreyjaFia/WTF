613f35ccf4cc247c230834739c1517b843358ab3
Alain Elemia <alain.elemia@gmail.com>
2025-12-02 19:13:07 +0800

    Added MAUI project

---
+++ src/WTF.MAUI/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WTF.MAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/WTF.MAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace WTF.MAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
