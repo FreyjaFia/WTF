d2293699eb1465f17cb260d7ccff012755f4d791
Alain Elemia <alain.elemia@gmail.com>
2026-02-18 05:26:40 +0800

    Read user id from JWT subject
    Use 'sub' claim to extract user GUID and throw UnauthorizedAccessException when missing or invalid.

--- src/WTF.Api/Common/Extensions/ClaimsPrincipalExtensions.cs
+++ src/WTF.Api/Common/Extensions/ClaimsPrincipalExtensions.cs
@@ -1,3 +1,4 @@
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 
 namespace WTF.Api.Common.Extensions;
@@ -6,8 +7,8 @@ public static class ClaimsPrincipalExtensions
 {
     public static Guid GetUserId(this ClaimsPrincipal principal)
     {
-        var userIdClaim = principal.FindFirst(ClaimTypes.NameIdentifier);
-        
+        var userIdClaim = principal.FindFirst(JwtRegisteredClaimNames.Sub);
+
         if (userIdClaim == null || !Guid.TryParse(userIdClaim.Value, out var userId))
         {
             throw new UnauthorizedAccessException("User ID not found in token claims");
